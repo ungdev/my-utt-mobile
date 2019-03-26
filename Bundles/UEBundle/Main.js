347f21c58ad7fbc6a5dc4ad763eb25cd60a05830
Arnaud9145 <arnaud.dufour@utt.fr>
2019-03-26 19:15:03 +0100

    removed setting screen

--- Bundles/UEBundle/Main.js
+++ Bundles/UEBundle/Main.js
@@ -8,7 +8,6 @@ import TabBarIcon from '../../components/TabBarIcon'
 import MyUEScreen from './screens/MyUEScreen'
 import SearchUEScreen from './screens/SearchUEScreen'
 import UEDetailsScreen from './screens/UEDetailsScreen'
-import SettingsScreen from './screens/SettingsScreen'
 
 const MyUEsStack = createStackNavigator({
   MyUEs: MyUEScreen,
@@ -18,33 +17,18 @@ const SearchStack = createStackNavigator({
   Search: SearchUEScreen,
   Details: UEDetailsScreen
 })
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen
-})
 
 MyUEsStack.navigationOptions = {
   tabBarLabel: 'Mes UEs',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name='briefcase' />
-  )
+  tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name='briefcase' />
 }
 
 SearchStack.navigationOptions = {
   tabBarLabel: 'Rechercher',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name='search' />
-  )
-}
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Catalogue',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name='book' />
-  )
+  tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name='search' />
 }
 
 export default createBottomTabNavigator({
   MyUEsStack,
-  SearchStack,
-  SettingsStack
+  SearchStack
 })
